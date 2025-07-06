7fa259c26e8b40f0f4c5bf19f4198a313452c7ff
AlekseyNovolodskiy <147639812+AlekseyNovolodskiy@users.noreply.github.com>
2025-07-06 20:15:11 +0300

    change swagger

--- src/main/java/task_manager/testing/controllers/TaskUserController.java
+++ src/main/java/task_manager/testing/controllers/TaskUserController.java
@@ -1,5 +1,7 @@
 package task_manager.testing.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -28,14 +30,21 @@ public class TaskUserController {
     public void updateTask(@RequestBody TaskUserDto taskDto, Authentication jwtauth){
         taskService.updateTask(taskDto,jwtauth.getName());
     }
+    @Operation(summary = "просмотр всех своих задач")
     @GetMapping(SHOW_TASK)
     public  List<TaskDto> showTasks(Authentication jwtauth){
         return taskService.showAllTasks(jwtauth.getName());
     }
+    @Operation(summary = "просмотр всех комментариев")
+    @Parameter(name = "имя задачи", description = "имя задачи, коментарии, которой мы хотим просмотреть", example = "task2")
     @GetMapping(SHOW_COMMENTS)
     public List<CommentsDto> showComments (@RequestParam String taskName, Authentication jwtauth){
        return taskService.showTasksComments(taskName,jwtauth.getName());
     }
+
+    @Operation(summary = "оставляем комментарий к своему проекту")
+    @Parameter(name = "имя задачи", description = "имя задачи, где мы хотим оставить коментарий", example = "task2")
+    @Parameter(name = "коментарий", description = "оставляем сам коментарий", example = "все сделал,я молодец")
     @GetMapping(LEAVE_COMMENTS)
     public void leaveComments (@RequestParam String taskName, @RequestParam String comment, Authentication jwtauth){
         taskService.leaveTasksComments(taskName,comment,jwtauth.getName());
